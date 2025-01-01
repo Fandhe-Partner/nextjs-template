d5ee5ffba708b515f57410b1038f9e316dc76b62
aLiz-Nancy <nancy@alizlab.com>
2025-01-01 18:41:27 +0900

    fix: update storybook configration

--- .storybook/preview.js
+++
@@ -1,5 +0,0 @@
-import "../src/app/globals.scss";
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-};
--- src/components/button/index.stories.tsx
+++ src/components/button/index.stories.tsx
@@ -1,14 +1,25 @@
+import { action } from "@storybook/addon-actions";
 import type { Meta, StoryObj } from "@storybook/react";
 import Button from "./index";
 
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    onClick: { action: "clicked" },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    children: "Button",
+    onClick: action("Clicked"),
+  },
 };
